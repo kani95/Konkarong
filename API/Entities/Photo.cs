8ef2cce66f93796c75b678a082158211b99a4a18
kasni <kasni95work@outlook.com>
2024-01-01 17:33:44 +0100

    Extended Api, added automapper, added seed for generating users to db

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities;
+
+[Table("Photos")]
+public class Photo
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string PublicId { get; set; }
+
+    // to not be nullable in migrations
+    // auto delete photo when user is deleted
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
+}
