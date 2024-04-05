54a8003eba70643dc1592ec1c55207700e3b0c3f
kasni <kasni95work@outlook.com>
2024-04-05 15:32:13 +0200

    Added feature for deleting, sending messages for users

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -10,10 +10,8 @@ import { Message } from '../_models/message';
 export class MessageService {
 
   baseUrl = environment.apiUrl;
-
-
+  
   constructor(private http: HttpClient) { }
-
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = getPaginationHeaders(pageNumber, pageSize);
@@ -21,5 +19,17 @@ export class MessageService {
     params = params.append("Container", container);
     
     return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+  }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content});
+  }
+
+  deleteMessage(id: number) {
+    return this.http.delete(this.baseUrl + 'messages/' + id);
   }
 }
